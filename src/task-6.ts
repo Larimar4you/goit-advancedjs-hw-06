1d8dd74e17efb681b27a516f01421ae8fafeb520
Lara Kosta AI × Fullstack <larissakost23@gmail.com>
2026-06-16 22:34:08 +0200

    Initial commit

---
+++ src/task-6.ts
@@ -0,0 +1,10 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+const num = getFirstElement<number>([1, 2, 3]);
+const str = getFirstElement<string>(["a", "b", "c"]);
+const bool = getFirstElement<boolean>([true, false, true]);
+
+console.log(num, str, bool);
+// 1 'a' true
